d1a7eff0dc7874276ca5206ac00a187f51b2fa13
Liquidd <luis_rojo240@hotmail.com>
2018-07-19 15:34:04 -0700

    terminados SP alta productos y alta venta

--- sql/recompensas.sql
+++ sql/recompensas.sql
@@ -3,6 +3,9 @@ historial.nombre_usuario,historial.accion,historial.cantidad,historial.valor as 
 INNER JOIN productos ON productos.id_producto = historial.id_producto
 WHERE historial.id_usuario = 87
 
+SELECT COUNT( id_producto ) AS total FROM historial GROUP BY id_producto ORDER BY total DESC
+
+
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `alta_producto`(IN `_modelo` VARCHAR(50), IN `_marca` VARCHAR(50), IN `_categoria` VARCHAR(50), IN `_descripcion` VARCHAR(100), IN `_cantidad` TINYINT(4), IN `_precio` INT(10), OUT `respuesta` VARCHAR(100))
     NO SQL
@@ -31,3 +34,35 @@ declare _modelo_producto int;
 	commit;
 END$$
 DELIMITER ;
+
+bloq:BEGIN
+declare vr_cantidad_existente int;
+declare vr_precio int;
+declare vr_total_a_pagar int;
+declare vr_cantidad_restante int;
+
+	SET vr_precio := (SELECT precio FROM productos  WHERE id_producto = _id_producto);
+	SET vr_cantidad_existente := (SELECT cantidad_existente  FROM productos  WHERE id_producto = _id_producto);
+	SET vr_total_a_pagar := (vr_precio * _cantidad_comprada);
+	SET vr_cantidad_restante := (vr_cantidad_existente - _cantidad_comprada);
+	start transaction;
+		begin
+	      	IF  vr_cantidad_existente < _cantidad_comprada THEN
+			  	rollback;
+			 	SET respuesta = 'No existe suficiente cantidad para la venta';
+			 	leave bloq;
+			ELSEIF  _pago <  vr_total_a_pagar THEN
+		    	rollback;
+		        SET respuesta = 'No Tienes Suficientes Puntos';
+		        leave bloq;
+	    	ELSE
+				INSERT INTO historial(id_cliente,id_producto,cantidad_comprada,total_pagado)VALUES(_id_cliente,_id_producto,_cantidad_comprada,vr_total_a_pagar);
+				UPDATE productos SET cantidad_existente = vr_cantidad_restante WHERE id_producto = _id_producto;
+				IF vr_cantidad_restante <= 0 THEN
+					UPDATE productos SET estado = 0 WHERE id_producto = _id_producto;
+				END IF;				
+				SET respuesta = 'Nueva Compra Registrado';
+	    	END IF;
+    	END;
+	commit;
+END
